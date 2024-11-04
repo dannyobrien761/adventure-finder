6ac567b52a17784fd23723c8e1e0b133203d736f
Danny O'Brien <dannyobrien761@gmail.com>
2024-11-04 23:54:29 +0000

    added models to admin.py and created an author and a blog post

--- adventures/admin.py
+++ adventures/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Author
 from .models import Post
+from .models import Comment
+
 # Register your models here.
+admin.site.register(Author)
 admin.site.register(Post)
+admin.site.register(Comment)
