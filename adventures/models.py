f9262077252697e8573aace66da7b02438f079b6
Danny O'Brien <dannyobrien761@gmail.com>
2024-11-13 13:52:40 +0000

    added tag and junction models postTag to models.py to add a tag to posts to allow for better filtering options

--- adventures/models.py
+++ adventures/models.py
@@ -22,6 +22,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
+    tags = models.ManyToManyField('Tag', through='PostTag', related_name='posts')
+
     class meta:
             ordering = ["-created_on"]
 
@@ -41,4 +43,28 @@ class Comment(models.Model):
         ordering = ["created_at"]
     def __str__(self):
         return f"Comment by {self.content} by {self.user}"
+
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True) 
+    tag_type = models.CharField(max_length=50, unique=True)  #tag type should be hiking, location, advert,  
+    description = models.TextField(blank=True, null=True)  
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tag_type
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # Optional field for tracking when the relationship was added
+
+    class Meta:
+        unique_together = ('post', 'tag')  # Composite primary key ensuring unique post-tag pairs
+        verbose_name = 'Post Tag'  # For display purposes in admin, optional
+        verbose_name_plural = 'Post Tags'
+
+    def __str__(self):
+        return f"{self.post.title} - {self.tag.name}"
+
 
